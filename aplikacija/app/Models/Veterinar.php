4e1dcdbde98a428b5fc8e4062f07d1bf4a8c9c61
Maja364 <majaaleksic62@gmail.com>
2023-01-21 13:00:27 +0100

    kreiran i povezan model

---
+++ aplikacija/app/Models/Veterinar.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Veterinar extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'ime',
+        'prezime',
+         
+    ];
+
+
+    public function pregledi()
+    {
+       return $this->hasMany(Pregled::class);
+    }
+}
